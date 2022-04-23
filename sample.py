1b33b97a09036e26cc574a237a35c7c2ddd35f08
Burak ÖZKAN <burak.ozkan@flo.com.tr>
2022-04-23 14:27:03 +0300

    episode1

--- sample.py
+++ sample.py
@@ -11,7 +11,8 @@ from curses import wrapper
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_WHITE)
-    curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    
     stdscr.clear()
     stdscr.addstr(1, 0, "Hello world!")
     stdscr.refresh()
